418a086fdaba2f602078b55ac02e6ba3bbdc00b9
Dimas Wahyu Sejati <160837117+DimasWahyuSejati@users.noreply.github.com>
2025-01-05 09:50:47 +0700

    Add files via upload

---
+++ modeldb.py
@@ -0,0 +1,8 @@
+from . import db
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    role = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+
